fb4538a5b5a142c02525a29460d4c1348580e25a
pkavoo <pkavoo@gmail.com>
2019-06-27 02:11:41 +0300

    UI Changes

---
+++ app/src/main/java/com/example/driverapp/PendingRequests.java
@@ -0,0 +1,15 @@
+package com.example.driverapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PendingRequests extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pending_requests);
+    }
+}
